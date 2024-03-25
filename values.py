f92a2ab102846ed9d4a8f2d0d2d7bded735057c6
RDO16 <91622126+RDO16@users.noreply.github.com>
2024-03-25 18:34:05 +0300

    Add files via upload

---
+++ values.py
@@ -0,0 +1,5 @@
+all_chars = [chr(i) for i in range(48, 58)]  # Numbers 0-9
+all_chars += [chr(i) for i in range(65, 91)]  # Uppercase letters A-Z
+all_chars += [chr(i) for i in range(97, 123)]  # Lowercase letters a-z
+
+print(all_chars)
